bd3ac86143e8a8c1cc1151ce3f636b14eed346c9
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-05-15 23:19:06 -0300

    Vehicle DTO created

---
+++ src/main/java/com/douglas/producer/controller/CarDTO.java
@@ -0,0 +1,14 @@
+package com.douglas.producer.controller;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CarDTO {
+
+    private String id;
+    private String model;
+    private String color;
+
+}
